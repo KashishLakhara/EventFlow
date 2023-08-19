6fd18adf1897bfdfe7ab6538c76a563ed688cbcc
Kashish Lakhara <kashiish19@gmail.com>
2023-08-19 20:12:49 +0530

    created route handlers

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,22 @@
-import express from 'express'
-import { json } from 'body-parser'
+import express from 'express';
+import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const app = express();
 app.use(json());
 
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
 app.listen(3000, () => {
     console.log('Listening to port 3000');
 });
+
+// create routes folder
+// add routes
+// import and use routes in index.ts
